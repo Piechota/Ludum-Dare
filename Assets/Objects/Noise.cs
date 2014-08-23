5cd0b16da40e00eb5a23bffe876d277851aa091e
Piechota <piechota33@gmail.com>
2014-08-24 00:26:57 +0200

    little change

---
+++ Assets/Objects/Noise.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+//dodane przez kube - aktualnie nieuzywane
+class Noise
+{
+    private float mean;
+    private float diff;
+
+    public Noise(float mean)
+    {
+        this.mean = mean;
+        this.diff = 0;
+    }
+
+    public void change(float alpha, float beta)
+    {
+        diff = diff * alpha + Mathf.Sin(3 * Time.time) * beta;
+    }
+    
+    public float get()
+    {
+        return mean + diff *Time.deltaTime;
+    }
+}
--- Assets/Player/Spells/Telekinesis.cs
+++ Assets/Player/Spells/Telekinesis.cs
@@ -5,8 +5,15 @@ public class Telekinesis : Spell {
 
     TelekinesisObject selected;
     Vector2 startMouse = Vector2.zero;
+    Vector3 distanceToObject;
 
-    float minDistanse = 0.5f;
+    float minDistance = 0.5f;
+    float maxDistance = 2.5f;
+    float maxObjectSpeed = 3.0f;
+    float maxObjectRotateSpeed = 5.0f;
+
+    //Noise directionNoise = new Noise(0);
+
     public Telekinesis(GameObject player) : base(player) { }
 
     public override void SpellUpdate()
@@ -22,7 +29,7 @@ public class Telekinesis : Spell {
             {
                 Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
                 RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
+                if (Physics.Raycast(ray, out hit, maxDistance))
                 {
                     if (hit.collider.GetComponent<TelekinesisObject>())
                     {
@@ -30,6 +37,8 @@ public class Telekinesis : Spell {
                         selected.transform.position += new Vector3(0.0f, 0.1f, 0.0f);
                         selected.GetComponent<Rigidbody>().useGravity = false;
                         startMouse = Vector2.zero;
+
+                        distanceToObject = selected.transform.position - player.transform.position;
                     }
                     else { /*tu bedzie jakiś dzwiek, particle itp oznaczajace niepowodzenie*/}
                 }
@@ -48,12 +57,20 @@ public class Telekinesis : Spell {
 
             if(selected)
             {
+                //directionNoise.change(0.99f, 0.1f);
+                //float randomDirection = directionNoise.get();
+                /*if (Input.GetKeyDown(KeyCode.W) ||
+                    Input.GetKeyDown(KeyCode.S) ||
+                    Input.GetKeyDown(KeyCode.D) ||
+                    Input.GetKeyDown(KeyCode.A))
+                    selected.transform.position = player.transform.position + distanceToObject;
+                */
                 selected.rigidbody.velocity = Vector3.zero;
-                
 
                 Vector3 direction = Vector3.zero;
 
-                float angle = startMouse.x / 100;
+                float angle = startMouse.x / (100 * selected.rigidbody.mass);
+                angle = (angle > maxObjectRotateSpeed) ? maxObjectRotateSpeed : angle;
                 selected.transform.RotateAround(player.transform.position, Vector3.up, angle);
 
                 
@@ -67,16 +84,21 @@ public class Telekinesis : Spell {
                 {
                     direction = Vector3.up;
                 }
-                direction *= startMouse.y / 130;
+                direction *= startMouse.y / (130 * selected.rigidbody.mass * selected.rigidbody.mass);
+
+                direction = (direction.magnitude > maxObjectSpeed) ? (direction.normalized * maxObjectSpeed) : direction;
 
                 Vector3 selectedPosition = selected.transform.position + direction;
-                selectedPosition = ((selectedPosition - player.transform.position).magnitude < minDistanse) ? selected.transform.position : selectedPosition;
+                selectedPosition = ((selectedPosition - player.transform.position).magnitude < minDistance) ? selected.transform.position : selectedPosition;
+                selectedPosition = ((selectedPosition - player.transform.position).magnitude > maxDistance) ? selected.transform.position : selectedPosition;
                 selected.rigidbody.MovePosition(selectedPosition);
 
                 Vector3 playerLookAt = selected.transform.position - player.transform.position;
                 playerLookAt.y = 0.0f;
                 player.transform.LookAt(player.transform.position + playerLookAt);
-                Camera.main.transform.LookAt(selected.transform);
+                Camera.main.transform.LookAt(selected.transform.position);
+                // + new Vector3(Mathf.Sin(randomDirection),  Mathf.Cos(Mathf.PI/2 - , 0)
+                distanceToObject = selected.transform.position - player.transform.position;
             }
         }
     }
