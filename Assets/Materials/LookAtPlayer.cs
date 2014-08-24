66de9b7ef8a216c828a4f4a8111964d1e648d0cc
Elejdor <elejdor@gmail.com>
2014-08-24 19:00:33 +0200

    no message

--- Assets/Materials/LookAtPlayer.cs
+++ Assets/Materials/LookAtPlayer.cs
@@ -11,7 +11,8 @@ public class LookAtPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.rotation = Quaternion.LookRotation(playerTransform.position - this.transform.position);
+
+        transform.LookAt(new Vector3(playerTransform.position.x, this.transform.position.y, playerTransform.position.z));
         Debug.DrawRay(this.transform.position, playerTransform.position - this.transform.position);
             //.SetLookRotation(playerTransform.position - this.transform.position);
 	}
