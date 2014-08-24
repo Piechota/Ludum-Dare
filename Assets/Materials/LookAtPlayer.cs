65019885028413e7c813bf8794b292b0749de1ad
Elejdor <elejdor@gmail.com>
2014-08-24 12:35:16 +0200

    added portal base

---
+++ Assets/Materials/LookAtPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    public Transform playerTransform;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.rotation = Quaternion.LookRotation(playerTransform.position - this.transform.position);
+        Debug.DrawRay(this.transform.position, playerTransform.position - this.transform.position);
+            //.SetLookRotation(playerTransform.position - this.transform.position);
+	}
+}
---
+++ Assets/Materials/PullRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullRotation : MonoBehaviour {
+
+    public Transform target;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.rotation = target.rotation;
+	}
+}
