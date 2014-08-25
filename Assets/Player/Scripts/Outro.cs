5ba739e2d775ef793c340541e5200e1420d44205
Piechota <piechota33@gmail.com>
2014-08-26 01:45:30 +0200

    some shit

---
+++ Assets/Player/Scripts/Outro.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Outro : MonoBehaviour {
+
+    public GameObject outro;
+
+	void OnTriggerEnter(Collider player)
+    {
+        if (player.tag != "Player")
+            return;
+
+        outro.SetActive(true);
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Player/Scripts/PlayOutro.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOutro : MonoBehaviour {
+
+    private bool wasPlayeng = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (audio.isPlaying)
+            return;
+
+        if(wasPlayeng)
+        {
+            Application.Quit();
+        }
+
+        wasPlayeng = true;
+        audio.Play();
+	}
+}
