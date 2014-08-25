90f0c927eee0852b632c71f61353a9e5f97aebe5
Elejdor <elejdor@gmail.com>
2014-08-25 21:06:17 +0200

    added intro prefab

---
+++ Assets/IntroScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroScript : MonoBehaviour {
+    public GameObject playerDesert;
+
+    public AudioClip dialog;
+    public float dialogLength = 2;
+    public AudioClip impacts;
+    public float impactsLength = 2;
+    private bool finished = false;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(playIntro());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (finished)
+        {
+            playerDesert.SetActive(true);
+            Debug.Log("Game starts");
+            Destroy(this.gameObject);
+        }
+	}
+
+    IEnumerator playIntro()
+    {
+        this.audio.Stop();
+        this.audio.clip = dialog;
+        this.audio.Play();
+        yield return new WaitForSeconds(dialogLength);
+        this.audio.Stop();
+        Debug.Log("First clip finished");
+
+        this.audio.Play();
+        this.audio.clip = impacts;
+        yield return new WaitForSeconds(impactsLength);
+        this.audio.Stop();
+        Debug.Log("Second clip finished");
+        finished = true;
+    }
+}
